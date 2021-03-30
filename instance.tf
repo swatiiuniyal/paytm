407736fddb4c63e13b58177556cf877b4f107626
Swati Uniyal <t-swati.uniyal@ocltp.com>
2021-03-31 05:26:34 +0530

    ec2 file

--- instance.tf
+++ instance.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "web2" {
   ami           =var.ami_id
   instance_type =var.instance_type
   subnet_id     =var.subnet_id
+  vpc_security_group_ids=[var.sg_id]
+  
 
   tags = {
     Name = "Assignment"
